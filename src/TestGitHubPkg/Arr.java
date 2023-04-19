5460fdaafee1edbc08860a2149a0ff4629785685
Padmaja.N <padmaja.nujella@gmail.com>
2023-04-19 18:50:50 +0530

    JavaGitHub commit

---
+++ src/TestGitHubPkg/Arr.java
@@ -0,0 +1,12 @@
+package TestGitHubPkg;
+
+public class Arr {
+
+	public static void main(String[] args) {
+		String[] colors= {"red","green","blue","pink"};
+		for(String s: colors) {
+			System.out.print(s+" ");
+		}
+	}
+
+}
